2cadde73bfecb47d7857319cc04439b4faf8ce7d
steve0079 <49959235+steve0079@users.noreply.github.com>
2019-09-11 12:11:32 -0700

    Updates

---
+++ precache-manifest.492f9b783c4a791d9405bf891620c4d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc05dce48c4efdafb034d71e9235097",
+    "url": "/dog-api/index.html"
+  },
+  {
+    "revision": "1eeae7d2db7bc88c747d",
+    "url": "/dog-api/static/css/main.164bea86.chunk.css"
+  },
+  {
+    "revision": "b3efe2a7793fc8b34bad",
+    "url": "/dog-api/static/js/2.a46e4f5d.chunk.js"
+  },
+  {
+    "revision": "1eeae7d2db7bc88c747d",
+    "url": "/dog-api/static/js/main.d6c05501.chunk.js"
+  },
+  {
+    "revision": "9f91a63f870acaea4ad6",
+    "url": "/dog-api/static/js/runtime~main.2c22ab41.js"
+  }
+]);
